5d28680bf9710b6377902cbbaa27ffe0040797b6
Supriya Rudroju <83066985+supriya855@users.noreply.github.com>
2024-02-04 16:48:44 +0000

    DatesInJs&num_math

---
+++ 01_Basics/05_num_math.js
@@ -0,0 +1,16 @@
+const score =500;
+console.log(score);
+
+const myNumber = new Number(100);
+console.log(myNumber.toString());
+console.log(myNumber.toString().length);
+console.log(myNumber.toFixed(2));
+
+const otherNumber=12.894763;
+console.log(otherNumber.toPrecision(3));
+const hundreads=10000000;
+console.log(hundreads.toLocaleString());//It takes by default USD range
+console.log(hundreads.toLocaleString('en-IN'));
+
+
+
---
+++ 01_Basics/06_DatesinJs.js
@@ -0,0 +1,26 @@
+//Dates
+let myDate = new Date();// we are creating an object here and myDate is an instance
+console.log(myDate);//2024-02-04T12:23:24.511Z
+console.log(myDate.toString());//Sun Feb 04 2024 12:23:24 GMT+0000 (Coordinated Universal Time)
+console.log(myDate.toDateString());//Sun Feb 04 2024
+console.log(myDate.toISOString());//2024-02-04T12:23:24.511Z
+console.log(myDate.toJSON());//2024-02-04T12:25:54.894Z
+console.log(myDate.toLocaleDateString());//2/4/2024
+console.log(myDate.toLocaleTimeString());//12:25:54 PM
+console.log(myDate.toTimeString());//12:25:54 GMT+0000 (Coordinated Universal Time)
+console.log(myDate.toUTCString());//Sun, 04 Feb 2024 12:25:54 GMT
+
+let myCreatedDate = new Date(2024,0,1);// here by this you can understand that month are starts with 0 --> january
+console.log(myCreatedDate);//2024-01-01T00:00:00.000Z
+let myCreatedDateIs = new Date(2024,0,2,5,30);
+console.log(myCreatedDateIs.toLocaleString());//1/2/2024, 5:30:00 AM
+
+let myTimeStamp = Date.now();
+console.log(myTimeStamp);
+console.log(myCreatedDate.getTime());
+console.log(Math.floor(Date.now()/1000));
+
+let newMyTime = new Date(2024,2,14);
+console.log(newMyTime.getDay());
+console.log(newMyTime.getMonth());
+newMyTime.toLocaleString('default', {'weekday':"long"});
