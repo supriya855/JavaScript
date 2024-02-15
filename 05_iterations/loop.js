8066cef30677db1483b2e11fa78edb48830bc65a
Supriya Rudroju <83066985+supriya855@users.noreply.github.com>
2024-02-15 17:41:56 +0000

    Intro to loops

---
+++ 05_iterations/loop.js
@@ -0,0 +1,28 @@
+//for -loop
+ length=5;
+for(let i=0;i<length;i++){
+    console.log(i+1);
+}
+array=[1,2,4,7,8];
+
+for(let i=0;i<array.length;i++){
+    console.log(array[i]);
+}
+
+for(let i=0;i<5;i++){
+    for(let j=0;j<5;j++){
+        console.log(i+j);
+    }
+}
+
+len=5,i=0;
+while(i<len){
+    console.log(i);
+    i++;
+}
+score = 1;
+do{
+    console.log("MyScore is "+score);
+    score++;
+}while(score<=5)
+
